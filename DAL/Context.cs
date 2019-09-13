aa16d90785e684e60746f875addc5a6544c9cc6b
Hannah Wilson <hw601315@ohio.edu>
2019-09-13 10:35:52 -0400

    change connection string

--- DAL/Context.cs
+++ DAL/Context.cs
@@ -14,6 +14,13 @@ namespace hw601315_SD1.DAL
          // this method is a 'constructor' and is called when a new context is created
          // the base attribute says which connection string to use
         }
+
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<OrderDetail> OrderDetails { get; set; }
+
+
     }
     
     // Include each object here. The value inside <> is the name of the class,
